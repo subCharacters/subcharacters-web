634e785125cb048afccb0fd98558684cc441f854
subCharacters <wldns59@naver.com>
2025-06-15 00:16:51 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider, Box, Heading, Text, Button } from '@chakra-ui/react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Box textAlign="center" py={20} px={6}>
+        <Heading as="h1" size="2xl" mb={4}>
+          subCharacters에 오신 걸 환영합니다!
+        </Heading>
+        <Text fontSize="lg" mb={6}>
+          Chakra UI로 만든 첫 정적 페이지입니다.
+        </Text>
+        <Button colorScheme="teal" size="lg">
+          시작하기
+        </Button>
+      </Box>
+    </ChakraProvider>
   );
 }
 
